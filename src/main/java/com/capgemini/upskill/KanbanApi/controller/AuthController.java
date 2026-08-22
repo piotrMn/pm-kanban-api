b11bb9c491dad46c448da047a9d0752ab191bce6
PM <piotr.muehleisen@gmail.com>
2026-08-22 22:50:12 +0200

    impl

--- src/main/java/com/capgemini/upskill/KanbanApi/controller/AuthController.java
+++ src/main/java/com/capgemini/upskill/KanbanApi/controller/AuthController.java
@@ -54,16 +54,4 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return ResponseEntity.badRequest().body(errors);
-    }
-
 }
--- src/main/java/com/capgemini/upskill/KanbanApi/controller/GlobalExceptionHandler.java
+++ src/main/java/com/capgemini/upskill/KanbanApi/controller/GlobalExceptionHandler.java
@@ -3,19 +3,24 @@ package com.capgemini.upskill.KanbanApi.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
+@SuppressWarnings("NullableProblems")
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @SuppressWarnings("NullableProblems")
     public ErrorResponse handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
         return new ErrorResponse() {
             @Override
@@ -26,6 +31,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             @Override
             public ProblemDetail getBody() {
                 return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid parameter type: " + ex.getName());
+            }
+        };
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        return new ErrorResponse() {
+            @Override
+            public HttpStatusCode getStatusCode() {
+                return HttpStatus.BAD_REQUEST;
+            }
+
+            @Override
+            public ProblemDetail getBody() {
+                Map<String, String> errors = new HashMap<>();
+                ex.getBindingResult().getAllErrors().forEach((error) -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+                return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation failed: " + errors);
             }
         };
     }
